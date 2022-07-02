3c675f5b5217bdffb084b6e1e8dee140c163b902
Indrajit <indrajitghosh912@gmail.com>
2022-07-02 23:56:04 +0530

    plotting spectral distribution of a matrix

--- plot_linear_alg.py
+++ plot_linear_alg.py
@@ -6,40 +6,35 @@
 #
 
 from manim import *
+from linear_algebra import *
 
 
-class SinAndCosFunctionPlot(Scene):
+class SpectralDistribution(Scene):
+
     def construct(self):
-        axes = Axes(
-            x_range=[-10, 10.3, 1],
-            y_range=[-1.5, 1.5, 1],
-            x_length=10,
-            axis_config={"color": GREEN},
-            x_axis_config={
-                "numbers_to_include": np.arange(-10, 10.01, 2),
-                "numbers_with_elongated_ticks": np.arange(-10, 10.01, 2),
-            },
-            tips=False,
-        )
-        axes_labels = axes.get_axis_labels()
-        sin_graph = axes.plot(lambda x: np.sin(x), color=BLUE)
-        cos_graph = axes.plot(lambda x: np.cos(x), color=RED)
 
-        sin_label = axes.get_graph_label(
-            sin_graph, "\\sin(x)", x_val=-10, direction=UP / 2
-        )
-        cos_label = axes.get_graph_label(cos_graph, label="\\cos(x)")
-
-        vert_line = axes.get_vertical_line(
-            axes.i2gp(TAU, cos_graph), color=YELLOW, line_func=Line
-        )
-        line_label = axes.get_graph_label(
-            cos_graph, "x=2\pi", x_val=TAU, direction=UR, color=WHITE
+        # Creating the number plane
+        number_plane = NumberPlane(
+            x_range=[-10, 10, 1],
+            y_range=[-10, 10, 1],
+            background_line_style={
+                "stroke_color": TEAL,
+                "stroke_width": 4,
+                "stroke_opacity": 0.4
+            }
         )
 
-        plot = VGroup(axes, sin_graph, cos_graph, vert_line)
-        labels = VGroup(axes_labels, sin_label, cos_label, line_label)
-        self.add(plot, labels)
+        # Unit circle
+        unit_circle = Circle(radius=1, color=GREY)
+
+        # Generating random unitary matrix
+        H = RandomMatrix(order=20)
+
+        # Plotting the eigenvalues of the matrix
+        complex_nums = [np.array([eig.real, eig.imag, 0]) for eig in H.eigen_values()]
+        dots = VGroup(*[Dot(num, color=RED, radius=0.05) for num in complex_nums])
+
+        self.add(number_plane, unit_circle, dots)
 
 
 def main():
