fd5ab4178d7aa8dd38b561ffd1f5b3078950dd8d
Indrajit <indrajitghosh912@gmail.com>
2022-07-02 23:01:43 +0530

    gitignore modified

---
+++ plot_linear_alg.py
@@ -0,0 +1,14 @@
+# Script to plot stuffs from linear_algebra
+#
+# Author: Indrajit Ghosh
+#
+# Date: Jul 02, 2022
+#
+
+
+def main():
+    print('hello world!')
+
+
+if __name__ == '__main__':
+    main()
