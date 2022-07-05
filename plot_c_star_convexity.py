4765ae01e6a53594de0be58495b6c2db45b56f58
Indrajit <indrajitghosh912@gmail.com>
2022-07-05 12:00:31 +0530

    modified

--- plot_c_star_convexity.py
+++ plot_c_star_convexity.py
@@ -18,22 +18,41 @@ from c_star_convexity import get_c_star_convex_combination
 # Change the matrix below
 INPUT_MATRIX = Matrix([
     [-1-1j, 0, 2],
-    [0, 3*1j, 0],
+    [0, 5*1j, 0],
     [0, 0, 2 + 1j]
 ])      
 
 
-LENGTH_OF_C_STAR_COMBINATION = 1
+LENGTH_OF_C_STAR_COMBINATION = 3
 
 ####################################################
 ####################################################
 
 
-class CStarConvexEigenValues(Scene):
-
+class PlotEigenValues(Scene):
+    
     def construct(self):
 
         # Creating the number plane
+        plane = self.get_number_plane()
+        
+
+        mat = INPUT_MATRIX # Input matrix
+
+        # Plotting the eigenvalues of the matrix
+        eigs_mat = self.get_eigenvalues_as_complex_num(mat)
+        eigval_color = RED
+        dots_mat = self.create_dots_from_complex_nums(eigs_mat, color=eigval_color)
+        txt = VGroup(*[
+            MathTex("\\lambda_{0}".format(i+1), font_size=35).next_to(dots_mat[i], UP).set_color(eigval_color) for i in range(len(eigs_mat))
+        ])
+        polygon = Polygram(eigs_mat)
+
+        self.add(plane, polygon, dots_mat, txt)
+
+
+    def get_number_plane(self):
+
         number_plane = NumberPlane(
             x_range=[-10, 10, 1],
             y_range=[-10, 10, 1],
@@ -44,24 +63,8 @@ class CStarConvexEigenValues(Scene):
             }
         )
 
-        D = INPUT_MATRIX # Input matrix
-        D_cc = get_c_star_convex_combination(D, LENGTH_OF_C_STAR_COMBINATION) # c_star combination
+        return number_plane
 
-        # Plotting the eigenvalues of the matrix D
-        eigs_D = self.get_eigenvalues_as_complex_num(D)
-        eigval_color = RED
-        dots_D = self.create_dots_from_complex_nums(eigs_D, color=eigval_color)
-        txt = VGroup(*[
-            MathTex("\\lambda_{0}".format(i+1), font_size=35).next_to(dots_D[i], UP).set_color(eigval_color) for i in range(len(eigs_D))
-        ])
-        polygon = Polygram(eigs_D)
-
-        # Plotting for the c-star convex combination
-        eigs_D_cc = self.get_eigenvalues_as_complex_num(D_cc)
-        dots_D_cc = self.create_dots_from_complex_nums(eigs_D_cc, color=YELLOW)
-
-        self.add(number_plane, polygon, dots_D, txt, dots_D_cc)
-    
 
     @staticmethod
     def get_eigenvalues_as_complex_num(mat:Matrix):
@@ -79,18 +82,38 @@ class CStarConvexEigenValues(Scene):
         return VGroup(*[Dot(num, radius=0.05, **kwargs) for num in complex_nums])
 
 
+
+class CStarConvexEigenValues(Scene):
+
+    def construct(self):
+
+        # Creating the number plane
+        plane = PlotEigenValues.get_number_plane(self)
+
+        D = INPUT_MATRIX # Input matrix
+        D_cc = get_c_star_convex_combination(D, LENGTH_OF_C_STAR_COMBINATION) # c_star combination
+
+        # Plotting the eigenvalues of the matrix D
+        eigs_D = PlotEigenValues.get_eigenvalues_as_complex_num(D)
+        eigval_color = RED
+        dots_D = PlotEigenValues.create_dots_from_complex_nums(eigs_D, color=eigval_color)
+        txt = VGroup(*[
+            MathTex("\\lambda_{0}".format(i+1), font_size=35).next_to(dots_D[i], UP).set_color(eigval_color) for i in range(len(eigs_D))
+        ])
+        polygon = Polygram(eigs_D)
+
+        # Plotting for the c-star convex combination
+        eigs_D_cc = PlotEigenValues.get_eigenvalues_as_complex_num(D_cc)
+        dots_D_cc = PlotEigenValues.create_dots_from_complex_nums(eigs_D_cc, color=YELLOW)
+
+        self.add(plane, polygon, dots_D, txt, dots_D_cc)
+
+
+
 class CubeRootsCStarConvex(Scene):
     def construct(self):
         # Creating the number plane
-        number_plane = NumberPlane(
-            x_range=[-10, 10, 1],
-            y_range=[-10, 10, 1],
-            background_line_style={
-                "stroke_color": TEAL,
-                "stroke_width": 4,
-                "stroke_opacity": 0.4
-            }
-        )
+        number_plane = PlotEigenValues.get_number_plane(self)
 
         # Unit circle
         unit_circle = Circle(radius=1, color=GREY)
@@ -100,8 +123,8 @@ class CubeRootsCStarConvex(Scene):
         D_cc = get_c_star_convex_combination(D, LENGTH_OF_C_STAR_COMBINATION) # c_star combination
 
         # Plotting the eigenvalues of the matrix D
-        eigs_D = CStarConvexEigenValues.get_eigenvalues_as_complex_num(D)
-        dots_D = CStarConvexEigenValues.create_dots_from_complex_nums(eigs_D, color=RED)
+        eigs_D = PlotEigenValues.get_eigenvalues_as_complex_num(D)
+        dots_D = PlotEigenValues.create_dots_from_complex_nums(eigs_D, color=RED)
         txt = VGroup(*[
             MathTex("1").next_to(eigs_D[0], RIGHT),
             MathTex("\\omega").next_to(eigs_D[1], UP),
@@ -110,8 +133,8 @@ class CubeRootsCStarConvex(Scene):
         triangle = Polygram(eigs_D)
 
         # Plotting for the c-star convex combination
-        eigs_D_cc = CStarConvexEigenValues.get_eigenvalues_as_complex_num(D_cc)
-        dots_D_cc = CStarConvexEigenValues.create_dots_from_complex_nums(eigs_D_cc, color=YELLOW)
+        eigs_D_cc = PlotEigenValues.get_eigenvalues_as_complex_num(D_cc)
+        dots_D_cc = PlotEigenValues.create_dots_from_complex_nums(eigs_D_cc, color=YELLOW)
 
         self.add(number_plane, unit_circle, triangle, dots_D, txt, dots_D_cc)
 
