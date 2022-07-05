88a28431fce4b518d8eb391a420f862a632e3785
Indrajit <indrajitghosh912@gmail.com>
2022-07-05 05:48:21 +0530

    get_c_star_convex_combination() created

--- c_star_convexity.py
+++ c_star_convexity.py
@@ -12,13 +12,31 @@ def get_c_star_convex_combination(mat:Matrix, length:int=2):
     """
     Returns a random C-star convex combination of the given matrix of 
     certain `length`.
+
+    Returns
+    -------
+        `Matrix`
     """
-    k = length
-    
+
+    if not mat.is_square():
+        raise Exception("The matrix is not square!")
+    else:
+        n, k = mat.rows, length
+
+        U = HaarDistributedUnitary(size=n*k) # Generating a random unitary of size `nk by nk`
+        mat_amplified = BlockDiagonalMatrix.amplify(mat=mat, num_of_times=k) # amplifying the matrix
+
+        M = U * mat_amplified * U.star()
+
+        # Returning the upper-left block of size `n`
+        return M.get_upper_left_block(size=n)
 
 
 def main():
-    print('hello world!')
+
+    pass
+
+    
 
 
 if __name__ == '__main__':
