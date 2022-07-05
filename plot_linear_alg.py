655481f29de6861fbcc65ccc7bf671814b17256a
Indrajit <indrajitghosh912@gmail.com>
2022-07-05 06:26:39 +0530

    modified

--- plot_linear_alg.py
+++ plot_linear_alg.py
@@ -7,6 +7,7 @@
 
 from manim import *
 from linear_algebra import *
+from c_star_convexity import get_c_star_convex_combination
 
 
 class SpectralDistribution(Scene):
@@ -27,18 +28,38 @@ class SpectralDistribution(Scene):
         # Unit circle
         unit_circle = Circle(radius=1, color=GREY)
 
-        # Generating random unitary matrix
-        R = RandomMatrix(order=20)
-        H = HaarDistributedUnitary(size=10)
-        P = RandomDensityMatrix(size=10)
+        # Generating matrix
+        cube_roots_of_unity = VandermondeMatrix.roots_of_unity()
+        D = DiagonalMatrix(cube_roots_of_unity)
 
-        mat = P # Change here
+        D_cc = get_c_star_convex_combination(D, 2)
 
-        # Plotting the eigenvalues of the matrix
-        complex_nums = [np.array([eig.real, eig.imag, 0]) for eig in mat.eigen_values()]
-        dots = VGroup(*[Dot(num, color=RED, radius=0.05) for num in complex_nums])
+        # Plotting the eigenvalues of the matrix D
+        eigs_D = self.get_eigenvalues_as_complex_num(D)
+        dots_D = self.create_dots_from_complex_nums(eigs_D, color=RED)
+        triangle = Polygram(eigs_D)
 
-        self.add(number_plane, unit_circle, dots)
+        # Plotting for the c-star convex combination
+        eigs_D_cc = self.get_eigenvalues_as_complex_num(D_cc)
+        dots_D_cc = self.create_dots_from_complex_nums(eigs_D_cc, color=YELLOW)
+
+        self.add(number_plane, unit_circle, dots_D, triangle, dots_D_cc)
+    
+
+    @staticmethod
+    def get_eigenvalues_as_complex_num(mat:Matrix):
+        """
+        Returns the eigenvalues for plotting on the complex plane
+
+        Returns
+        -------
+            `[np.array([lam.real, lam.imag, 0]) for lam in mat.eigen_values()]`
+        """
+        return [np.array([eig.real, eig.imag, 0.0]) for eig in mat.eigen_values()]
+
+    @staticmethod
+    def create_dots_from_complex_nums(complex_nums, **kwargs):
+        return VGroup(*[Dot(num, radius=0.05, **kwargs) for num in complex_nums])
 
 
 def main():
